308bcf4326cd29d4853a00dc1f497b7b691ade56
Recep Batuhan Dikmen <rbdikmen@gmail.com>
2022-07-25 17:28:23 +0300

    Workspace created

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  const blog = { id: 1, title: "Blog title", description: "Blog description" };
+
+  res.send(blog);
+});
+
+const port = 3000;
+
+app.listen(port, function () {
+  console.log(`Server listening on port ${port}`);
+});
