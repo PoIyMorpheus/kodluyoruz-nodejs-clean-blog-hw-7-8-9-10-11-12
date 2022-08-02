72cfc11a33f52fd1dc76a32ea8b3644b187e3d2d
Recep Batuhan Dikmen <rbdikmen@gmail.com>
2022-08-02 23:08:38 +0300

    Stage 5

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const methodOverride = require("method-override");
+
 const ejs = require("ejs");
 const Post = require("./models/Post");
-
+const pageController = require("./controllers/pageController");
+const postController = require("./controllers/postController");
 const app = express();
 
 //connect DB
@@ -19,29 +22,22 @@ app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.use(
+  methodOverride("_method", {
+    methods: ["POST", "GET"],
+  })
+);
+
 //ROUTES
-app.get("/", async (req, res) => {
-  const posts = await Post.find({});
-  res.render("index", {
-    posts,
-  });
-});
-app.get("/posts/:id", async (req, res) => {
-  const post = await Post.findById(req.params.id);
-  res.render("post", {
-    post,
-  });
-});
-app.get("/about", (req, res) => {
-  res.render("about");
-});
-app.get("/add_post", (req, res) => {
-  res.render("add_post");
-});
-app.post("/posts", async (req, res) => {
-  await Post.create(req.body);
-  res.redirect("/");
-});
+app.get("/", postController.getAllPosts);
+app.get("/posts/:id", postController.getPost);
+app.put("/posts/:id", postController.updatePost);
+app.delete("/posts/:id", postController.deletePost);
+app.post("/posts", postController.createPost);
+
+app.get("/about", pageController.getAboutPage);
+app.get("/add_post", pageController.getAddPostPage);
+app.get("/posts/edit/:id", pageController.getEditPage);
 
 const port = 3000;
 
